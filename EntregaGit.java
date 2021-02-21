ea83880052b3aef6c6de8f37156fbaf47fb9d7f7
Adans Lima <adans.lima@fatec.sp.gov.br>
2021-02-20 21:46:51 -0300

    Agora vai

---
+++ EntregaGit.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class EntregaGit{
+    public static void main(String[] args){
+        String nome = JOptionpane.showInputDialog("Qual seu nome?");
+        JOptionpane.showMessageDialog(null, "Bem-vindo, " + nome);
+        System.out.println("Entregando tarefas Git na linha de comando");
+        System.out.println("Salve Caraio V2");
+    }
+}
